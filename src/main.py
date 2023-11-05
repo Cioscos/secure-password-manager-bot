cebc72017293e1806166512c2a7c61e65812634b
Cioscos <claudiocicca1998@gmail.com>
2023-11-05 20:17:19 +0100

    Add log in the error_handler and disable sending pm on error

--- src/main.py
+++ src/main.py
@@ -1,8 +1,5 @@
-import json
 import secrets
 import string
-import traceback
-from pydoc import html
 from typing import List, Any
 from warnings import filterwarnings
 
@@ -104,36 +101,36 @@ async def error_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
     :param context: The Telegram context.
     """
     # Log the error before we do anything else, so we can see it even if something breaks.
-    # ic(f"Exception while handling an update: {context.error}")
+    logger.error(f"Exception while handling an update: {context.error}")
 
     # traceback.format_exception returns the usual python message about an exception, but as a
     # list of strings rather than a single string, so we have to join them together.
-    tb_list = traceback.format_exception(None, context.error, context.error.__traceback__)
-    tb_string = "".join(tb_list)
-
-    # Split the traceback into smaller parts
-    tb_parts = [tb_string[i: i + 4096] for i in range(0, len(tb_string), 4096)]
-
-    # Build the message with some markup and additional information about what happened.
-    update_str = update.to_dict() if isinstance(update, Update) else str(update)
-    base_message = (
-        f"An exception was raised while handling an update\n"
-        f"<pre>update = {html.escape(json.dumps(update_str, indent=2, ensure_ascii=False))}"
-        "</pre>\n\n"
-        f"<pre>context.chat_data = {html.escape(str(context.chat_data))}</pre>\n\n"
-        f"<pre>context.user_data = {html.escape(str(context.user_data))}</pre>\n\n"
-    )
-
-    # Send base message
-    await context.bot.send_message(
-        chat_id=keyring_get('DevId'), text=base_message, parse_mode=ParseMode.HTML
-    )
-
-    # Send each part of the traceback as a separate message
-    for part in tb_parts:
-        await context.bot.send_message(
-            chat_id=keyring_get('DevId'), text=f"<pre>{html.escape(part)}</pre>", parse_mode=ParseMode.HTML
-        )
+    # tb_list = traceback.format_exception(None, context.error, context.error.__traceback__)
+    # tb_string = "".join(tb_list)
+    #
+    # # Split the traceback into smaller parts
+    # tb_parts = [tb_string[i: i + 4096] for i in range(0, len(tb_string), 4096)]
+    #
+    # # Build the message with some markup and additional information about what happened.
+    # update_str = update.to_dict() if isinstance(update, Update) else str(update)
+    # base_message = (
+    #     f"An exception was raised while handling an update\n"
+    #     f"<pre>update = {html.escape(json.dumps(update_str, indent=2, ensure_ascii=False))}"
+    #     "</pre>\n\n"
+    #     f"<pre>context.chat_data = {html.escape(str(context.chat_data))}</pre>\n\n"
+    #     f"<pre>context.user_data = {html.escape(str(context.user_data))}</pre>\n\n"
+    # )
+    #
+    # # Send base message
+    # await context.bot.send_message(
+    #     chat_id=keyring_get('DevId'), text=base_message, parse_mode=ParseMode.HTML
+    # )
+    #
+    # # Send each part of the traceback as a separate message
+    # for part in tb_parts:
+    #     await context.bot.send_message(
+    #         chat_id=keyring_get('DevId'), text=f"<pre>{html.escape(part)}</pre>", parse_mode=ParseMode.HTML
+    #     )
 
 
 async def send_welcome_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
